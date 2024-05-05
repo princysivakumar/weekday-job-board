974e3d274480c1d50e1daee04db2682b1e8e522b
Princy-s <73104552+princysivakumar@users.noreply.github.com>
2024-05-05 18:37:03 +0530

    cards ui completed

--- weekday-job-board/src/components/cards.js
+++ weekday-job-board/src/components/cards.js
@@ -1,6 +1,8 @@
 import { Card } from '@mui/material';
 import React from 'react';
-import './cards.css'
+import './cards.css';
+import Button from '@mui/material/Button';
+import BoltIcon from '@mui/icons-material/Bolt';
 
 
 // import Card from '@mui/material/Card';
@@ -39,21 +41,24 @@ const JobCard = ({ job }) => {
 
 
         <div className="job-card-info">
-          <h2>{companyName}</h2> {/* Assuming company name is missing from UI */}
-          <h3>{jobRole}</h3> {/* Assuming job title is missing from UI */}
-          <p>{location}</p> {/* Assuming location is missing from UI */}
+          <div className='company-name'>{companyName}</div> 
+          <div className='job-role'>{jobRole || 'All'}</div> 
+          <div className='location'>{location}</div>
         </div>
       </div>
+      <div className='estimate-salary'>Estimated Salary: <span>
+          {minJdSalary && `${minJdSalary}-${maxJdSalary} ${salaryCurrencyCode}`} 
+        </span></div>
       <div className="job-card-description">
-        <p>{jobDetailsFromCompany.slice(0, 150)}...</p> {/* Truncate description */}
+        <div className='about-company'>About Company:</div>
+        <p>{jobDetailsFromCompany}</p> {/* Truncate description */}
       </div>
+      <div className='min-exp'>  Minimum Experience:</div>
+        <div className='exp-value'>
+          {minExp} - {maxExp || 'No Limit'} years 
+        </div>
       <div className="job-card-footer">
-        <span>
-          {minJdSalary && `${minJdSalary}-${maxJdSalary} ${salaryCurrencyCode}`} {/* Assuming salary range is missing from UI */}
-        </span>
-        <span>
-          {minExp} - {maxExp || 'No Limit'} years exp
-        </span>
+      <Button className='easy-apply-button' variant="contained" startIcon={<BoltIcon />}>Easy Apply</Button>
         {/* Conditionally render "Easy Apply" button based on the image  */}
         {/* This section might need adjustment based on actual button presence*/}
         {/* {isEasyApplyButtonVisible && <button>Easy Apply</button>} */}
